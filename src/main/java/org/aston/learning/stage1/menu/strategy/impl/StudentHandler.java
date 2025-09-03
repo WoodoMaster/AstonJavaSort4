fa3d81a2fa27b2faf0d655fdd3eba25f2250d887
Alexs1051 <buxarik2016@yandex.ru>
2025-09-03 16:14:05 +0200

    feature: Реализация ввода с проверкой на соответствие регулярному выражению  - Добавлен новый метод readRegex в InputUtils;  - Добавлен новый enum - InputPattern, с заготовленными шаблонами EMAIL, NAME, PASSWORD и т.д.
    refactor:
     - Во всех стратегиях ElementHandler - readString заменен на readRegex.

--- src/main/java/org/aston/learning/stage1/menu/strategy/impl/StudentHandler.java
+++ src/main/java/org/aston/learning/stage1/menu/strategy/impl/StudentHandler.java
@@ -2,15 +2,16 @@ package org.aston.learning.stage1.menu.strategy.impl;
 
 import org.aston.learning.stage1.model.Student;
 import org.aston.learning.stage1.menu.strategy.ElementHandler;
+import org.aston.learning.stage1.util.console.InputPattern;
 import org.aston.learning.stage1.util.console.InputUtils;
 
 public class StudentHandler implements ElementHandler<Student> {
     @Override
     public Student createElementManually() {
         System.out.println("Создание студента:");
-        String group = InputUtils.readString("Группа: ", true);
+        String group = InputUtils.readRegex("Группа*: ", InputPattern.STUDENT_GROUP, true);
         double average = InputUtils.readDouble("Средний балл: ", 0.0, 5.0);
-        String recordBook = InputUtils.readString("Номер зачетки: ", true);
+        String recordBook = InputUtils.readRegex("Номер зачетки*: ", InputPattern.RECORD_BOOK, true);
         return new Student(group, average, recordBook);
     }
 
---
+++ src/main/java/org/aston/learning/stage1/util/console/InputPattern.java
@@ -0,0 +1,47 @@
+package org.aston.learning.stage1.util.console;
+
+public enum InputPattern {
+    // Email
+    EMAIL("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+            "Неверный формат email! Пример: name@mail.ru"),
+
+    // Группа студента
+    STUDENT_GROUP("^\\d{2}-[A-z]{2,4}(-\\d{1,2}){0,2}$",
+            "Неверный формат группы! Пример: 22-Vt, 22-SUd-1"),
+
+    // Номер зачетки
+    RECORD_BOOK("^\\d{2}-\\d{2}\\\\\\d{1,2}$",
+            "Неверный формат зачетки! Пример: 12-39\\6"),
+
+    // Модель автобуса
+    BUS_MODEL("^[A-z]{2,4}\\d{0,2}(-\\d{1,2})?$",
+            "Неверный формат модели! Пример: VAZ1, Liaz"),
+
+    // Номер автобуса
+    BUS_NUMBER("^\\d{2,3}[A-Z]?$",
+            "Неверный формат номера! Пример: 47, 53B"),
+
+    // Имя
+    NAME("^[A-Za-zА-Яа-я\\s-]{2,50}$",
+            "Имя должно содержать только буквы и быть от 2 до 50 символов"),
+
+    // Пароль
+    PASSWORD("^.{6,20}$",
+            "Пароль должен быть от 6 до 20 символов");
+
+    private final String regex;
+    private final String errorMessage;
+
+    InputPattern(String regex, String errorMessage) {
+        this.regex = regex;
+        this.errorMessage = errorMessage;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
